a162f01f7e8a0713c7d451946b656999cd0edce4
HEXS <aswameda18@gmail.com>
2025-09-16 17:57:51 +0800

    Update forum.js

--- forum.js
+++ forum.js
@@ -14,15 +14,17 @@ navItem.forEach((nav) => {
 });
 
 
-const menuToggle = document.querySelector(".navbar-toggle");
 
-menuToggle.addEventListener("click", (e) => {
-    document.querySelector(".navbar-group").classList.toggle("navbar-expand");
-    document.querySelector(".navbar-action").classList.toggle("navbar-expand");
+const menuToggle = document.querySelector(".burger");
+const navbarGroup = document.querySelector(".navbar-group");
+const navbarAction = document.querySelector(".navbar-action");
 
-    e.target.classList.toggle("navbar-expand");
+menuToggle.addEventListener("click", () => {
+    navbarGroup.classList.toggle("navbar-expand");
+    navbarAction.classList.toggle("navbar-expand");
+
+    menuToggle.classList.toggle("toggle-burger");
 });
-
 
 
 // like animasi
@@ -38,5 +40,65 @@ document.querySelectorAll(".like-icon").forEach(likeIcon => {
     } else {
       likeCount.textContent = count - 1;
     }
+  });
+});
+
+
+
+
+// sidebar
+const sidebar = document.querySelector(".sidebar");
+    const toggleBtn = document.querySelector(".sidebar-toggle-btn");
+    const toggleIcon = toggleBtn.querySelector("i");
+
+
+    toggleBtn.addEventListener("click", () => {
+      sidebar.classList.toggle("active");
+      updateIcon();
+    });
+
+    function updateIcon() {
+      if (sidebar.classList.contains("active")) {
+        toggleIcon.classList.replace("fa-chevron-right", "fa-chevron-left");
+      } else {
+        toggleIcon.classList.replace("fa-chevron-left", "fa-chevron-right");
+      }
+    }
+
+    let startX = 0;
+    let endX = 0;
+
+    document.addEventListener("touchstart", (e) => {
+      startX = e.touches[0].clientX;
+    });
+
+    document.addEventListener("touchend", (e) => {
+      endX = e.changedTouches[0].clientX;
+      handleSwipe();
+    });
+
+    function handleSwipe() {
+      let diff = endX - startX;
+
+      // Geser kanan → buka sidebar
+      if (diff > 50) {
+        sidebar.classList.add("active");
+        updateIcon();
+      }
+      // Geser kiri → tutup sidebar
+      else if (diff < -50) {
+        sidebar.classList.remove("active");
+        updateIcon();
+      }
+    }
+
+
+const menuItems = document.querySelectorAll(".sidebar .menu-item");
+
+menuItems.forEach(item => {
+  item.addEventListener("click", () => {
+    menuItems.forEach(i => i.classList.remove("active"));
+
+    item.classList.add("active");
   });
 });
